094425fc1a99342951660d900e4717b163de8401
202100649@estudantes.ips.pt <202100649@estudantes.ips.pt>
2022-05-26 20:28:08 +0100

    visuals done in main game

--- GameIPM/Assets/Scripts/Jogo/GridSpace.cs
+++ GameIPM/Assets/Scripts/Jogo/GridSpace.cs
@@ -8,8 +8,11 @@ public class GridSpace : MonoBehaviour
     
     public Button button;
     public Text buttonText;
+    public GameObject cross;
+    public GameObject skull;
 
     private GameController gameController;
+
 
     public void SetGameControllerReference(GameController controller)
     {
@@ -20,10 +23,47 @@ public class GridSpace : MonoBehaviour
     {
         if (gameController.playerMove == true)
         {
+
             buttonText.text = gameController.GetPlayerSide();
+            if (buttonText.text == "X")
+            {
+
+                cross.SetActive(true);
+            } 
+
+            if (buttonText.text == "O")
+            {
+
+                skull.SetActive(true);
+            }
+
+            
             button.interactable = false;
             gameController.EndTurn();
+            
         }
+    }
+
+    private void Update()
+    {
+
+        if (buttonText.text == "X")
+        {
+           
+            cross.SetActive(true);
+        }
+
+        if (buttonText.text == "O")
+        {
+          
+            skull.SetActive(true);
+        }
+
+       if (buttonText.text == "")
+        {
+            cross.SetActive(false);
+            skull.SetActive(false);
+        } 
     }
 
 
